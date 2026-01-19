353479d91d30555ac7584677e76d9eed8b171fb2
php-prosvirin-dev <php.prosvirin.dev@gmail.com>
2026-01-19 12:24:07 +0300

    Routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,21 @@ use App\Http\Controllers\ActionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [ActionController::class, 'index'])->name('dashboard');
-    Route::post('/dashboard/check-in', [ActionController::class, 'checkIn'])->name('dashboard.check-in');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::get('/', [ActionController::class, 'index'])->name('index');
+        Route::post('/check-in', [ActionController::class, 'checkIn'])->name('check-in');
+    });
+
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
+    });
+
 });
 
 require __DIR__.'/auth.php';
